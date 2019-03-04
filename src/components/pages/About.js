6108fe082e8006e1de283ab000b4fb67f7f8fcb7
Eyder ACM <eyderacm@gmail.com>
2019-03-04 14:57:04 -0600

    create about component

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a Todo List application made to understand the fundamentals of React</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
